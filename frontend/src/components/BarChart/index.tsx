31f847432a501be5a51c765bd65470f586643eaf
Thiago Raniery Ignacio Brito <thiagobritotrs@gmail.com>
2021-05-07 16:01:54 -0300

    BarChart integration

--- frontend/src/components/BarChart/index.tsx
+++ frontend/src/components/BarChart/index.tsx
@@ -1,6 +1,35 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts';
+import { SaleSuccess } from 'types/sale';
+import { round } from 'utils/format';
+import { BASE_URL } from 'utils/requests';
+
+type SeriesData = {
+  name: string;
+  data: Array<number>
+}
+
+type ChartData = {
+  labels: {
+    categories: Array<string>;
+  },
+  series: Array<SeriesData>
+}
 
 export default function BarChart() {
+  const [chartData, setChartData] = useState<ChartData>({
+    labels: {
+      categories: []
+    },
+    series: [
+      {
+        name: "",
+        data: []
+      }
+    ]
+  });
+
   const options = {
     plotOptions: {
       bar: {
@@ -9,22 +38,30 @@ export default function BarChart() {
     }
   }
 
-  const mockData = {
-    labels: {
-      categories: ['Anakin', 'Obi-Wan', 'Luke', 'Padmé', 'Snape']
-    },
-    series: [
-      {
-        name: "% Sucesso",
-        data: [43.6, 67.1, 67.7, 45.6, 71.1]
-      }
-    ]
-  };
+  useEffect(() => {
+    axios.get(`${BASE_URL}/sales/success-by-seller`).then(response => {
+      const data = response.data as Array<SaleSuccess>
+      const myLabels = data.map(x => x.sellerName)
+      const mySeries = data.map(x => round(x.deals / x.visited * 100.0, 1))
+
+      setChartData({
+        labels: {
+          categories: myLabels
+        },
+        series: [
+          {
+            name: "% Sucesso",
+            data: mySeries
+          }
+        ]
+      });
+    });
+  }, []);
 
   return (
     <Chart
-      options={{ ...options, xaxis: mockData.labels }}
-      series={mockData.series}
+      options={{ ...options, xaxis: chartData.labels }}
+      series={chartData.series}
       type="bar"
       height={240}
     />
